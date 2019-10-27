8fb4bba4a04aa0371d84dab60a4344c6ec3d14d7
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-10-27 11:32:37 +0200

    Completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl: string;
+
+  constructor(private _http: Http) {
+  this._albumUrl = '../assets/album.json'
+  }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(response => response.json())
+  }
+}
