a55db234c9caa0f1ab48403bada9364fd7224e3a
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-10-27 11:40:12 +0200

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,10 +5,9 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  private _albumUrl: string;
+  private _albumUrl: string = '../assets/album.json';
 
   constructor(private _http: Http) {
-  this._albumUrl = '../assets/album.json'
   }
 
   getAlbum(id: number) {
